f81c26e64117b9bddc005724e9fd84d5c4e4a6e3
jafonsoc <jafonsocoutinho@protonmail.com>
2020-03-31 17:39:13 +0100

    Added core implementation of track and trace

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerImpl.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerImpl.java
@@ -2,10 +2,13 @@ package pt.tecnico.sauron.silo;
 
 import static io.grpc.Status.INVALID_ARGUMENT;
 
+import pt.tecnico.sauron.silo.domain.Observation;
 import io.grpc.stub.StreamObserver;
 import pt.tecnico.sauron.silo.domain.SiloServer;
 import pt.tecnico.sauron.silo.grpc.Silo.PingRequest;
 import pt.tecnico.sauron.silo.grpc.Silo.PingResponse;
+import pt.tecnico.sauron.silo.grpc.Silo.SearchRequest;
+import pt.tecnico.sauron.silo.grpc.Silo.ObservationMessage;
 import pt.tecnico.sauron.silo.grpc.SauronGrpc;
 
 public class SiloServerImpl extends SauronGrpc.SauronImplBase {
@@ -25,6 +28,22 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
             responseObserver.onNext(response);
             responseObserver.onCompleted();
         }
+    }
+
+    @Override
+    public void track(final SearchRequest request, final StreamObserver<ObservationMessage> responseObserver) {
+        final String type = request.getType();
+        final String id = request.getId();
+        Observation obs;
+
+        if (type.equals("person")) {
+            obs = siloServer.trackPerson(id);
+        } else if (type.equals("car")) {
+            obs = siloServer.trackCar(id);
+        }
+
+        //final ObservationMessage response = ObservationMessage.newBuilder()
+        //    .setId(id).setType(type).setTimestamp(obs.getTimestamp());
     }
 
 }
--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/SiloServer.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/SiloServer.java
@@ -1,17 +1,84 @@
 package pt.tecnico.sauron.silo.domain;
 
-import pt.tecnico.sauron.silo.grpc.Silo.ResponseMessage;
 
-import java.lang.String;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class SiloServer {
     private ArrayList<Camera> _cameras;
-    private ArrayList<Car> _observations;
+    private LinkedHashMap<String, ArrayList<Observation>> _cars;
+    private LinkedHashMap<String, ArrayList<Observation>> _persons;
 
     public SiloServer() {
         _cameras = new ArrayList<>();
-        _observations = new ArrayList<>();
+        _cars = new LinkedHashMap<>();
+        _persons = new LinkedHashMap<>();
+    }
+
+    // Track command
+    public Observation trackPerson(String id) {
+        return track(_persons, id);
+    }
+
+    public Observation trackCar(String id) {
+        return track(_cars, id);
+    }
+
+    // Having a generic method for command allows for adding more types of observations in the future
+    private Observation track(Map<String, ArrayList<Observation>> observations, String id) {
+        if (observations.containsKey(id)) {
+            ArrayList<Observation> list = observations.get(id);
+
+            // sorting is handled in insertion so the first element is always most recent
+            return list != null ? list.get(0) : null;
+        }
+
+        return null;
+    }
+
+    // Trace command
+    public List<Observation> tracePerson(String id) {
+        return _persons.get(id);
+    }
+
+    public List<Observation> traceCar(String id) {
+        return _cars.get(id);
+    }
+
+
+    // Track Match command
+    public List<Observation> trackMatchPerson(String id) {
+        return trackMatch(_persons, id);
+    }
+
+    public List<Observation> trackMatchCar(String id) {
+        return trackMatch(_cars, id);
+    }
+
+    // Having a generic method for command allows for adding more types of observations in the future
+    private List<Observation> trackMatch(Map<String, ArrayList<Observation>> observations, String expr) {
+        List<Observation> list = new ArrayList<>();
+
+        if (!expr.contains("*")) {
+            for (String id : observations.keySet()) {
+                if (id.contains(expr)) {
+                    // sorting is handled in insertion so the first element is always most recent
+                    list.add(observations.get(id).get(0));
+                }
+            }
+        } else {
+            String[] patterns = expr.split("'*'");// separate prefix and suffix
+
+            for (String id : observations.keySet()) {
+                if (id.startsWith(patterns[0]) && id.endsWith(patterns[1])) {
+                    list.add(observations.get(id).get(0));
+                }
+            }
+        }
+
+        return list;
     }
 
     @Override
@@ -19,8 +86,4 @@ public class SiloServer {
         return "hey";
     }
 
-    public ResponseMessage play() {
-        return ResponseMessage.CLEAR_FAIL;
-
-    }
 }
