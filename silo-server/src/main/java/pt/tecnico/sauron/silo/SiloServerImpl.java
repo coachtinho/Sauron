9c2d6aee0243107ddfecfd6dc1c23977c8c9d666
João Coutinho <j.afonso.coutinho@tecnico.ulisboa.pt>
2020-04-29 16:36:40 +0100

    Implemented ts verification in server

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerImpl.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerImpl.java
@@ -4,6 +4,7 @@ import static io.grpc.Status.INVALID_ARGUMENT;
 import static io.grpc.Status.ALREADY_EXISTS;
 
 import java.util.Vector;
+import java.util.List;
 
 import pt.tecnico.sauron.silo.domain.Observation;
 import io.grpc.stub.StreamObserver;
@@ -16,7 +17,6 @@ import pt.tecnico.sauron.silo.grpc.Silo.ReportResponse.FailureItem;
 import pt.tecnico.sauron.silo.grpc.SauronGrpc;
 import com.google.protobuf.Timestamp;
 
-import java.util.List;
 
 public class SiloServerImpl extends SauronGrpc.SauronImplBase {
 
@@ -27,25 +27,47 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
     public SiloServerImpl(int instance) {
         siloServer = new SiloServer();
         _instance = instance;
-        _valueTS = new Vector<Integer>(_instance); // valueTS starts big enough to include own instance
+        _valueTS = new Vector<Integer>();
+        for (int i = 0; i < _instance; i++) _valueTS.add(0); // valueTS starts big enough to include own instance
     }
 
-    public boolean canUpdate(Vector<Integer> otherTS) {
+    private Vector<Integer> generateOtherTS(List<Integer> tsList) {
+        // Get client's valueTS from list
+        Vector<Integer> otherTS = new Vector<Integer>();
+
+        for (int ts : tsList)
+            otherTS.add(ts);
+
         // Ensure both vectors have same size
         if (_valueTS.size() > otherTS.size())
-            otherTS.setSize(_valueTS.size());
+            for (int i = otherTS.size(); i < _valueTS.size(); i++)
+                otherTS.add(0);
         else if (_valueTS.size() < otherTS.size())
-            _valueTS.setSize(otherTS.size());
+            for (int i = _valueTS.size(); i < otherTS.size(); i++)
+                _valueTS.add(0);
 
-        for (int i = 0; i < _valueTS.size(); i++) {
-            // Initiate null values at 0
-            if (_valueTS.get(i) == null) _valueTS.setElementAt(0, i);
-            if (otherTS.get(i) == null) otherTS.setElementAt(0, i);
+        return otherTS;
+    }
 
+    private boolean canUpdate(Vector<Integer> otherTS) {
+
+        for (int i = 0; i < _valueTS.size(); i++)
             if (otherTS.get(i) > _valueTS.get(i)) return false;
-        }
             
         return true;
+    }
+
+    private Vector<Integer> updateTS(Vector<Integer> otherTS) {
+        // Merges ts and increments replica's own value
+        Vector<Integer> newTS = new Vector<Integer>();
+        
+        for (int i = 0; i < _valueTS.size(); i++)
+            newTS.add(Math.max(_valueTS.get(i), otherTS.get(i)));
+
+        int ts = newTS.get(_instance - 1) + 1;
+        newTS.setElementAt(ts, _instance - 1);
+
+        return newTS;
     }
 
     @Override
@@ -86,6 +108,7 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
         final String type = request.getType();
         final String id = request.getId();
         Observation obs;
+        Vector<Integer> otherTS = generateOtherTS(request.getTsList());
 
         if (type.isBlank()) {
             responseObserver.onError(INVALID_ARGUMENT.withDescription("Type cannot be empty!").asRuntimeException());
@@ -111,6 +134,10 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
         }
 
         TrackResponse.Builder responseBuilder = TrackResponse.newBuilder();
+                
+        _valueTS = updateTS(otherTS);
+        for (int ts : _valueTS)
+            responseBuilder.addTs(ts);
 
         if (obs != null) {
             Timestamp timestamp = Timestamp.newBuilder().setSeconds(obs.getInstant().getEpochSecond()).build();
@@ -132,6 +159,7 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
         final String type = request.getType();
         final String id = request.getId();
         List<Observation> observations;
+        Vector<Integer> otherTS = generateOtherTS(request.getTsList());
 
         if (type.isBlank()) {
             responseObserver.onError(INVALID_ARGUMENT.withDescription("Type cannot be empty!").asRuntimeException());
@@ -157,6 +185,10 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
         }
 
         TraceResponse.Builder responseBuilder = TraceResponse.newBuilder();
+                
+        _valueTS = updateTS(otherTS);
+        for (int ts : _valueTS)
+            responseBuilder.addTs(ts);
 
         if (observations != null && !observations.isEmpty()) {
 
@@ -184,6 +216,7 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
         final String type = request.getType();
         final String id = request.getId();
         List<Observation> observations;
+        Vector<Integer> otherTS = generateOtherTS(request.getTsList());
 
         if (type.isBlank()) {
             responseObserver.onError(INVALID_ARGUMENT.withDescription("Type cannot be empty!").asRuntimeException());
@@ -209,6 +242,10 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
         }
 
         TrackMatchResponse.Builder responseBuilder = TrackMatchResponse.newBuilder();
+                
+        _valueTS = updateTS(otherTS);
+        for (int ts : _valueTS)
+            responseBuilder.addTs(ts);
 
         if (observations != null && !observations.isEmpty()) {
 
@@ -234,15 +271,34 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
     @Override
     public void camJoin(final CameraRegistrationRequest request,
             final StreamObserver<CameraRegistrationResponse> responseObserver) {
+
         String name = request.getName();
+        Vector<Integer> otherTS = generateOtherTS(request.getTsList());
+
         if (name.isBlank()) // Check name exists
             responseObserver.onError(INVALID_ARGUMENT.withDescription("Name cannot be empty!").asRuntimeException());
         else if (name.length() > 15 || name.length() < 3) // Check name size
             responseObserver.onError(INVALID_ARGUMENT.withDescription("Name length is illegal!").asRuntimeException());
-        else {
+        else if (!canUpdate(otherTS)) {
+            CameraRegistrationResponse.Builder responseBuilder = CameraRegistrationResponse.newBuilder();
+
+            for (int ts : _valueTS)
+                responseBuilder.addTs(ts);
+
+            final CameraRegistrationResponse response = responseBuilder.build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } else {
             try {
                 siloServer.registerCamera(name, request.getLatitude(), request.getLongitude());
-                final CameraRegistrationResponse response = CameraRegistrationResponse.newBuilder().build();
+                
+                CameraRegistrationResponse.Builder responseBuilder = CameraRegistrationResponse.newBuilder();
+                
+                _valueTS = updateTS(otherTS);
+                for (int ts : _valueTS)
+                    responseBuilder.addTs(ts);
+
+                final CameraRegistrationResponse response = responseBuilder.build();
                 responseObserver.onNext(response);
                 responseObserver.onCompleted();
             } catch (SiloException e) {
@@ -256,6 +312,8 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
     public void camInfo(final CameraInfoRequest request, final StreamObserver<CameraInfoResponse> responseObserver) {
         String name = request.getName();
         Camera cam;
+        Vector<Integer> otherTS = generateOtherTS(request.getTsList());
+
         if (name.isBlank()) // Check name exists
             responseObserver.onError(INVALID_ARGUMENT.withDescription("Name cannot be empty!").asRuntimeException());
         else if ((cam = siloServer.camInfo(name)) == null) // Check name exists
@@ -263,8 +321,16 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
         else {
             double latitude = cam.getLatitude();
             double longitude = cam.getLongitude();
-            final CameraInfoResponse response = CameraInfoResponse.newBuilder() //
-                    .setLatitude(latitude).setLongitude(longitude).build();
+
+            CameraInfoResponse.Builder responseBuilder = CameraInfoResponse.newBuilder();
+                
+            _valueTS = updateTS(otherTS);
+            for (int ts : _valueTS)
+                responseBuilder.addTs(ts);
+
+            responseBuilder.setLatitude(latitude).setLongitude(longitude);
+
+            final CameraInfoResponse response = responseBuilder.build();
 
             responseObserver.onNext(response);
             responseObserver.onCompleted();
@@ -274,11 +340,28 @@ public class SiloServerImpl extends SauronGrpc.SauronImplBase {
     @Override
     public void report(final ReportRequest request, final StreamObserver<ReportResponse> responseObserver) {
         String cameraName = request.getCameraName();
+        Vector<Integer> otherTS = generateOtherTS(request.getTsList());
+        
         if (!siloServer.hasCamera(cameraName)) {
             responseObserver.onError(INVALID_ARGUMENT.withDescription("No such camera").asRuntimeException());
+        } else if (!canUpdate(otherTS)) {
+            ReportResponse.Builder responseBuilder = ReportResponse.newBuilder();
+
+            _valueTS = updateTS(otherTS);
+            for (int ts : _valueTS)
+                responseBuilder.addTs(ts);
+
+            ReportResponse response = responseBuilder.build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
         } else {
             List<ReportItem> items = request.getReportsList();
             ReportResponse.Builder responseBuilder = ReportResponse.newBuilder();
+
+            _valueTS = updateTS(otherTS);
+            for (int ts : _valueTS)
+                responseBuilder.addTs(ts);
+
             for (ReportItem item : items) {
                 String type = item.getType();
                 String id = item.getId();
